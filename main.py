9fb2998fd048c0bd71290ae145e153394b48270b
Alfist552 <ndetsura@mail.ru>
2025-12-08 12:32:48 +0300

    Создание класса Memory и функции setup_window для настройки базового окна игр

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print('Test')
+import tkinter as tk
+from tkinter import ttk
+import random
+
+class Memory:
+    def __init__(self, root):
+        self.root = root
+        self.setup_window()
+        self.initialize_variables()
+        self.interface()
+        self.start_new_game()
+
+    def setup_window(self):
+        try:
+            self.root.title("Memory game")
+            self.root.geometry("800x600")
+            self.root.resizatable(True, True)
+            self.root.protocol("WM_DELETE_WINDOW",self.on_closing)
+        except Exception as e:
+            print(f"Произошла ошибка при настройке")
+
