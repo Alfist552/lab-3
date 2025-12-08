312447913f366975f575f0a705f563079e173c90
Alfist552 <ndetsura@mail.ru>
2025-12-08 13:35:39 +0300

    Добавление класса initialize_variables для инициализации переменных

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 import random
+import os
 
 class Memory:
     def __init__(self, root):
@@ -18,4 +19,20 @@ class Memory:
             self.root.protocol("WM_DELETE_WINDOW",self.on_closing)
         except Exception as e:
             print(f"Произошла ошибка при настройке")
+            self.show_error_message("setup_window", e)
 
+    def initialize_variables(self):
+        try:
+            self.grid_size = 6
+            self.game_active = False
+            self.cards = []
+            self.flipped.cards = []
+            self.matched.pairs = 0
+            self.total_pairs = 0
+            self.moves = 0
+            self.card_images = []
+            self.card_back_image = None
+
+        except Exception as e:
+            print("Ошибка инициализации")
+            self.show_error_mesage("initialize_variables", e)
